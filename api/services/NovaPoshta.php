7b05ae3103aa5cdd1fd73a9fe809ee33b558051a
avantis <uaruscom@gmail.com>
2017-12-13 19:20:39 +0200

    Create NovaPoshta.php

---
+++ api/services/NovaPoshta.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of the API SHOP
+ *
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/pllano/api-shop
+ * @version 1.0.1
+ * @package pllano.api-shop
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+ 
+namespace Services\Delivery;
+ 
+class NovaPoshta
+{
+    public function request($message, array $arr = array())
+    {
+        // тело метода
+    }
+	
+    public function response($message, array $arr = array())
+    {
+        // тело метода
+    }
+}
