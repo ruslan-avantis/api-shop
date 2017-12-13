b1682825180deabd5c09e3c695d526e362dcce1d
avantis <uaruscom@gmail.com>
2017-12-13 20:01:17 +0200

    Update services.php

--- api/app/routers/services.php
+++ api/app/routers/services.php
@@ -10,3 +10,33 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+$app->post("/v1/json/services/{service:[\w]+}", function (Request $request, Response $response, array $args) {
+    $service = $request->getAttribute('service');
+    $param = $request->getParsedBody();
+    
+	if (isset($service)) {
+        $key = filter_var($param["request"]["key"], FILTER_SANITIZE_STRING);
+        if ($this->get("settings")["services"][$service] == $key) {
+            $service = ucfirst($service);
+            $resp = new $service($param);
+        } else {
+            // Доступ запрещен. Ключ доступа не совпадает.
+            $resp["headers"]["status"] = "401 Unauthorized";
+            $resp["headers"]["code"] = 401;
+            $resp["headers"]["message"] = "Access is denied";
+            $resp["headers"]["message_id"] = $this->get("settings")["http-codes"]."".$resp["headers"]["code"].".md";
+        }
+    } else {
+        // Сервис не определен. Возвращаем ошибку 400
+        $resp["headers"]["status"] = "404 Not Found";
+        $resp["headers"]["code"] = 404;
+        $resp["headers"]["message"] = "Bad Request";
+        $resp["headers"]["message_id"] = $this->get("settings")["http-codes"]."".$resp["headers"]["code"].".md";
+    }
+
+    echo json_encode($resp, JSON_PRETTY_PRINT);
+    return $response->withStatus(200)->withHeader("Content-Type","application/json");
+
+});
+ 
