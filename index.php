4e83f492ddd940d6c4c24fae1caabea93f6b9172
avantis <uaruscom@gmail.com>
2017-12-22 01:49:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,23 +12,23 @@
  */
 
 // Вывод ошибок. Что бы выключить закоментируйте эти строки
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+// ini_set('error_reporting', E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
 
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
     $url  = parse_url($_SERVER['REQUEST_URI']);
     $file = __DIR__ . $url['path'];
-	
+    
     if (is_file($file)) {
         return false;
     }
 }
 
 // Проверяем наличие файлов API Shop
-if (!file_exists(__DIR__ . '/app/test.php';)){
+if (!file_exists(__DIR__ . '/app/test.php')) {
 
 }
 
@@ -42,23 +42,24 @@ $loader->register();
 $loader->addNamespace('Pllano\ApiShop', __DIR__ . '/app/classes');
 
 // Подключаем Composer
-if (file_exists(__DIR__ . '/../vendor/autoload.php';)){
+if (file_exists(__DIR__ . '/../vendor/autoload.php')){
     require __DIR__ . '/../vendor/autoload.php';
-} elseif (file_exists(__DIR__ . '/vendor/autoload.php';)){
+} elseif (file_exists(__DIR__ . '/vendor/autoload.php')){
     require __DIR__ . '/vendor/autoload.php';
 } else {
     // Если autoload.php не найден - подключаем загрузчик пакетов
     require __DIR__ . '/app/installer.php';
+    
     $loaders = new \Pllano\ApiShop\Loader();
     // Запускаем загрузку пакетов и указываем директорию
     $load = $loaders->run(__DIR__ . '/../vendor');
-	
+    
     if ($load == true){
-	    require __DIR__ . '/vendor/autoload.php';
-	} else {
-	$error = new \Pllano\ApiShop\Error();
-	$error->permission();
-	}
+        require __DIR__ . '/vendor/autoload.php';
+    } else {
+        $error = new \Pllano\ApiShop\Error();
+        $error->permission();
+    }
 }
 
 require __DIR__ . '/app/config/settings.php';
@@ -77,14 +78,14 @@ session_start();
 
 $cores = glob(__DIR__ . '/app/core/*.php');
 foreach ($cores as $core) {
-require $core;
+    require $core;
 }
  
 // Automatically register routers
 // Автоматическое подключение роутеров
 $routers = glob(__DIR__ . '/app/routers/*.php');
 foreach ($routers as $router) {
-require $router;
+    require $router;
 }
 
 // Slim Run
