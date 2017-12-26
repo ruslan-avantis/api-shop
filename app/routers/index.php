f59366602ca1ac94d240b174ab97ab4792002a94
avantis <uaruscom@gmail.com>
2017-12-26 03:29:23 +0200

    Add files via upload

--- app/routers/index.php
+++ app/routers/index.php
@@ -13,16 +13,16 @@
  
 use Slim\Http\Request;
 use Slim\Http\Response;
- 
 use Adbar\Session;
 use Defuse\Crypto\Crypto;
- 
 use ApiShop\Config\Settings;
 use ApiShop\Utilities\Utility;
-use ApiShop\Model\SessionUser;
 use ApiShop\Resources\Language;
 use ApiShop\Resources\Menu;
 use ApiShop\Resources\Site;
+use ApiShop\Database\Router;
+use ApiShop\Database\Ping;
+use ApiShop\Model\SessionUser;
 
 $app->get('/', function (Request $request, Response $response, array $args) {
     // Получаем конфигурацию \ApiShop\Config\Settings
@@ -43,7 +43,7 @@ $app->get('/', function (Request $request, Response $response, array $args) {
         $lang = $site_config["language"];
     }
     // Подключаем мультиязычность
-	$language = (new Language())->get($lang);
+    $language = (new Language())->get($lang);
     // Подключаем плагины
     $utility = new Utility();
     // Генерируем токен
@@ -57,15 +57,69 @@ $app->get('/', function (Request $request, Response $response, array $args) {
         $session->authorize = Crypto::encrypt('0', $session_key);
         $authorize = 0;
     }
-    // Отдаем информацию о станице для правильной работы шаблонов
+    // Отдаем информацию для шаблонизатора
+    // Информацию о странице
     $page = ["page" => 'home'];
-    // Получаем данные Menu пользователя которые отдадим шаблонизатору Twig
-    $menu = (new Menu())->get();
- 
+    // Данные пользователя из сессии
+    $session_user_data =(new SessionUser())->get();
     // Что бы не давало ошибку присваиваем пустое значение
     $content = '';
     // print_r($content);
+ 
+    // Формируем параметры запроса
+    $arr = array(
+        "limit"            => 9,
+        "brand_id"        => '66',
+        "articul"        => '80x190'
+    );
+    // Подключаем ApiShop\Database\Router
+    $database = new Router((new Ping("price"))->get());
+    // Отправляем запрос и получаем данные
+    $response = $database->get("price", $arr);
+    // Если ответ не пустой
+    if (count($response['items']) >= 1) {
+        foreach($response['items'] as $item)
+        {
+            // Обрабатываем картинки
+            $product['image']['no_image'] = $utility->get_image(null, 'https://life24.com.ua/images/no_image.png', 360, 360);
+            $image_1 = '';
+            $image_1 = (isset($item['item']['image']['1'])) ? $utility->clean($item['item']['image']['1']) : null;
+            if (isset($image_1)) {$product['image']['1'] = $utility->get_image($item['item']['product_id'], $image_1, 360, 360);}
+            $image_2 = '';
+            $image_2 = (isset($item['item']['image']['2'])) ? $utility->clean($item['item']['image']['2']) : null;
+            if (isset($image_2)) {$product['image']['2'] = $utility->get_image($item['item']['product_id'], $image_2, 360, 360);}
 
+            $path_url = pathinfo($item['item']['url']);
+            $basename = $path_url['basename']; // lib.inc.php
+            $baseurl = str_replace('-'.$item['item']['product_id'].'.html', '', $basename);
+
+            $product['url'] = '/product/'.$item['item']['id'].'/'.$baseurl.'.html';
+            $product['name'] = (isset($item['item']['name'])) ? $utility->clean($item['item']['name']) : '';
+            $product['type'] = (isset($item['item']['type'])) ? $utility->clean($item['item']['type']) : '';
+            $product['brand'] = (isset($item['item']['brand'])) ? $utility->clean($item['item']['brand']) : '';
+            $product['serie'] = (isset($item['item']['serie'])) ? $utility->clean($item['item']['serie']) : '';
+            $product['articul'] = (isset($item['item']['articul'])) ? $utility->clean($item['item']['articul']) : '';
+            if ($item['item']['serie'] && $item['item']['articul']) {$product['name'] = $item['item']['serie'].' '.$item['item']['articul'];}
+            $product['oldprice'] = (isset($item['item']['oldprice'])) ? $utility->clean($item['item']['oldprice']) : '';
+            $product['price'] = (isset($item['item']['price'])) ? $utility->clean($item['item']['price']) : '';
+            $product['available'] = (isset($item['item']['available'])) ? $utility->clean($item['item']['available']) : '';
+
+            $rand = rand(1000, 5000);
+            $date = date("Y-m-d H:i:s",strtotime(date("Y-m-d H:i:s")." +".$rand." minutes"));
+            $date = strtotime($date);
+            $product['y'] = date("Y", $date);
+            $product['m'] = date("m", $date);
+            $product['d'] = date("d", $date);
+            $product['h'] = date("H", $date);
+            $product['i'] = date("i", $date);
+            $product['s'] = date("s", $date);
+            // Отдаем данные шаблонизатору 
+            $products['product'][] = $product;
+        }
+    } else {
+        $products = null;
+    }
+ 
     // Запись в лог
     $this->logger->info("home");
  
@@ -76,9 +130,10 @@ $app->get('/', function (Request $request, Response $response, array $args) {
         "config" => $config['settings']['site'],
         "language" => $language,
         "token" => $session->token,
+        "session" => $session_user_data,
         "session_temp" => $session_temp,
-        "menu" => $menu,
-        "content" => $content
+        "content" => $content,
+        "products" => $products
     ]);
  
 });
