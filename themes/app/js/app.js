e62dfb906db4b58d09fa31e4ebb89bea2d58d4a3
avantis <uaruscom@gmail.com>
2017-12-29 03:11:43 +0200

    Add files via upload

---
+++ themes/app/js/app.js
@@ -0,0 +1,263 @@
+// global lib
+var templates_lib = '/themes/lib'
+// api-shop lib
+var api_shop = '/themes/app'
+
+// Проверка доступности indexedDB
+var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB
+// Также могут отличаться и window.IDB* objects: Transaction, KeyRange и тд
+var IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction
+var IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
+
+// Открываем базу данных MyTestDatabase
+var db
+var request = indexedDB.open("apiShop", 1)
+request.onupgradeneeded = function(event) {
+    db = event.target.result
+    db.createObjectStore("languages", {keyPath: "id"})
+    db.createObjectStore("user", {keyPath: "id"})
+}
+
+var localDb = window.localStorage
+// Примеры использования
+// localDb.setItem('key', 'value');
+// var cat = localDb.getItem("key");
+// localDb.removeItem("key");
+// localDb.clear();
+
+var sessionDb = window.sessionStorage
+// Примеры использования
+// sessionDb.setItem('key', 'value');
+// var data = sessionDb.getItem('key');
+// sessionDb.removeItem('key');
+// sessionDb.clear();
+
+// Получаем локализацию в js
+var languages = new Object()
+languages = getLanguages()
+
+// Функция проверки доступности localStorage и sessionStorage
+function storageAvailable(type) {
+    try {
+        var storage = window[type],
+            x = '__storage_test__'
+        storage.setItem(x, x)
+        storage.removeItem(x)
+        return true
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            storage.length !== 0
+    }
+}
+
+// Пишем данные в localStorage, sessionStorage, indexedDB
+function setDb(key, data) {
+    if (storageAvailable('localStorage')) {
+        localDb.setItem(key, data)
+    }
+    if (storageAvailable('sessionStorage')) {
+        sessionDb.setItem(key, data)
+    }
+    // Сейчас мы только пишем в indexedDB
+    // Разбираюсь как удобно получать данные
+    request.onerror = function(event) {
+        //alert("Почему Вы не позволяете моему веб-приложению использовать IndexedDB?!")
+        return null
+    }
+    request.onsuccess = function(event) {
+        db = event.target.result
+        var tx = db.transaction(key, "readwrite")
+        var store = tx.objectStore(key)
+        for (var i in data) {
+            try {
+                //console.log(i, cData[i])
+                store.put(data[i])
+            } catch(e){
+                //alert(e.name)
+            }
+        }
+    }
+}
+
+function getDb(key) {
+    if (storageAvailable('localStorage')) {
+        return localDb.getItem(key)
+    }
+    else if (storageAvailable('sessionStorage')) {
+        return sessionDb.getItem(key)
+    }
+    else {
+        return null
+    }
+}
+
+function removeDb(key) {
+    if (storageAvailable('localStorage')) {
+        localDb.removeItem(key)
+        return true
+    }
+    else if (storageAvailable('sessionStorage')) {
+        sessionDb.removeItem(key)
+        return true
+    }
+    else {
+        return null
+    }
+}
+
+function clearDb() {
+    try {
+        localDb.clear()
+        sessionDb.clear()
+        return true
+    } catch(e){
+        // alert(e.name)
+        return null
+    }
+}
+
+function createCookie(name, value, days) {
+    if (days) {
+        var date = new Date()
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 365))
+        var expires = "; expires=" + date.toGMTString()
+    } else var expires = ""
+    document.cookie = name + "=" + value + expires + "; path=/"
+}
+
+function readCookie(name) {
+    var nameEQ = name + "="
+    var ca = document.cookie.split(';')
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i]
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length)
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length)
+    }
+    return null
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1)
+}
+
+function isValidJSON(src) {
+    var filtered = src
+    filtered = filtered.replace(/\\["\\\/bfnrtu]/g, '@')
+    filtered = filtered.replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']')
+    filtered = filtered.replace(/(?:^|:|,)(?:\s*\[)+/g, '')
+    return (/^[\],:{}\s]*$/.test(filtered))
+};
+
+function OneNotify(title, text, addclass) {
+    new PNotify({title: title, text: text, addclass: addclass})
+}
+
+function jsonNotify(json) {
+    new PNotify(json)
+}
+    
+function getLanguages() {
+    if(getDb('languages') != null) {
+        var json = getDb('languages')
+        var obj = JSON && JSON.parse(json) || $.parseJSON(json)
+        return obj
+    } else {
+        setLanguage(2)
+    }
+}
+
+function lang(id) {
+    var getLang = $.grep(languages, function(e){ return e.id == id })
+    return getLang[0].name
+}
+
+function setLanguage(id) {
+    var csrf = $("#csrf").val()
+    $.post("/language", {csrf: csrf, id: id}, function (response) {
+        var data = JSON && JSON.parse(response) || $.parseJSON(response)
+        if(data.status == "OK")
+        {
+            // Сохраняем массив с локализацией у юзера
+            try {
+                setDb('languages', JSON.stringify(data.languages))
+                window.location.reload()
+            } catch(e){
+                //testDb('languages')
+                window.location.reload()
+            }
+        } else if(data.status == "NO") {
+            OneNotify(data.title, data.text, data.color)
+        }
+    }),"json"
+}
+
+function checkIn() {
+    checkPhone('phone', lang(200), false)
+    checkPassword('password', lang(201), password_expression)
+    checkEmail('email', lang(161), email_expression)
+    checkData('fname', lang(57), word_expression)
+    checkData('iname', lang(58), word_expression)
+    var phone = $("#phone").intlTelInput("getNumber")
+    var email = $("#email").val()
+    var password = $("#password").val()
+    var iname = $("#iname").val()
+    var fname = $("#fname").val()
+    var csrf = $("#csrf").val()
+    $.post("/check-in", {email: email, phone: phone, password: password, iname: iname, fname: fname, csrf: csrf}, function (response) {
+        var data = $.parseJSON(response)
+        if(data.status == "OK") {
+            window.location = '/'
+        }
+        else if(data.status == "NO") {
+            OneNotify(data.title, data.text, data.color)
+        }
+    }
+    ),"json"
+    }
+
+function login() {
+    checkPhone('phone', lang(200), false)
+    checkPassword('password', lang(201), password_expression)
+    checkEmail('email', lang(161), email_expression)
+    var phone = $("#phone").intlTelInput("getNumber")
+    var email = $("#email").val()
+    var password = $("#password").val()
+    var csrf = $("#csrf").val()
+    $.post("/login", {email: email, phone: phone, password: password, csrf: csrf}, function (response) {
+        var data = $.parseJSON(response)
+        if(data.status == "OK") {
+            window.location = '/'
+        }
+        else if(data.status == "NO") {
+            OneNotify(data.title, data.text, data.color)
+        }
+    }
+    ),"json"
+}
+
+function logout() {
+    var csrf = $("#csrf").val()
+    $.post("/logout", {csrf: csrf}, function (response) {
+        var data = $.parseJSON(response)
+        if(data.status == "OK")
+        {
+            window.location = '/'
+        }
+        else if(data.status == "NO") {
+            OneNotify(data.title, data.text, data.color)
+        }
+    }
+    ),"json"
+}
+ 
---
+++ themes/app/js/check.js
@@ -0,0 +1,173 @@
+﻿var word_expression = new RegExp(/^[а-яА-Яa-zA-ZÀ-ÿ0-9а-щА-ЩЬьЮюЯяЇїІіЄєҐґ\/\n/:;.!'"?%&*-+=_,№#$€@\ \-]{1,500}$/);
+var check_expression_new = new RegExp(/^[а-яА-Яa-zA-ZÀ-ÿ0-9а-щА-ЩЬьЮюЯяЇїІіЄєҐґ\/\n/:;.!'"?%&*-+=_,№#$€@\ \-]{1,500}$/);
+var check_expression = new RegExp(/^[а-яА-Яa-zA-ZÀ-ÿ0-9а-щА-ЩЬьЮюЯяЇїІіЄєҐґ\/\n/:;.!'"?%&*-+=_,№#$€@\ \-]{1,500}$/);
+var email_expression = new RegExp(/^[-0-9a-z_\.]+@[-0-9a-z_\.]+\.[a-z]{2,6}$/i);
+var phone_expression = new RegExp(/^[\(\)\[\]\s\\\/\-0-9\+]{5,50}/i);
+var phone_pattern = new RegExp(/^((8|0|\+\d{1,2})[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/i);
+var password_expression = new RegExp(/^[^\s]{8,25}$/);
+var code_expression = new RegExp(/[0-9]{2}-[0-9]{2}-[0-9]{2}$/);
+var url_expression = new RegExp(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi);
+ 
+function checkData(fieldname, message_item, check_expression)
+{    
+    var data_string = $("#" + fieldname).val();
+    if(data_string.length == 0)
+    {
+        $("#" + fieldname + " ").after(
+            "<div class=error_message id=" + fieldname + "_di ><i class='fa fa-info-circle text-red-md' aria-hidden='true'></i> " 
+            + message_item 
+            + " " 
+            + lang(43) 
+            + "</div>"
+        );
+        errorStatus = true;
+        $('#' + fieldname).css('background', '#FFF9C5');
+    }
+    else
+    if(check_expression != false)
+    {
+        var result = check_expression.test(data_string);
+        if(result == false)
+        {
+            $("#" + fieldname).after(
+                "<div class=error_message id=" + fieldname + "_di ><i class='fa fa-info-circle text-red-md' aria-hidden='true'></i> " 
+                + lang(41) 
+                + " " 
+                + message_item 
+                + "</div>"
+            );
+            errorStatus = true;
+            $('#' + fieldname).css('background', '#FFF9C5');
+        }
+        else
+        {
+            $('#' + fieldname).css('background', '#fff');
+        }
+    }
+    else
+    {
+        $('#' + fieldname).css('background', '#fff');
+    }
+}
+ 
+function checkPassword(fieldname, message_item, password_expression)
+{
+    var data_string = $('#' + fieldname).val();
+    if(data_string.length == 0)
+    {
+        $('#' + fieldname + "-status").html(
+            '<div class=error_message id=' + fieldname + '_div><i class="fa fa-info-circle text-red-md" aria-hidden="true"></i> ' 
+            + lang(42) 
+            + ' '
+            + message_item 
+            + ' ' 
+            + lang(43) 
+            + '</div>'
+        );
+        errorStatus = true;
+        $('#' + fieldname).css('background', '#FFF9C5');
+    }
+    else
+    if(password_expression != false)
+    {
+        var result = password_expression.test(data_string);
+        if(result == false)
+        {
+            $('#' + fieldname + "-status").html(
+                '<div class=error_message id=' + fieldname + '_divx><i class="fa fa-info-circle text-red-md" aria-hidden="true"></i> '
+                + message_item
+                + ': '
+                + data_string+' ' 
+                + lang(44) 
+                + ' </div>'
+            );
+            errorStatus = true;
+            $('#' + fieldname).css('background', '#FFF9C5');
+        }
+    }
+    else
+    {
+        $('#' + fieldname).css('background', '#fff');
+    }
+}
+ 
+function checkPhone(fieldname, message_item, phone_expression)
+{
+    var data_string = $('#' + fieldname).val();
+    if(data_string.length == 0)
+    {
+        $('#' + fieldname + "-status").html(
+            '<div class=error_message id=' + fieldname + '_di ><i class="fa fa-info-circle text-red-md" aria-hidden="true"></i> ' 
+            + lang(42) 
+            + ' '
+            + message_item
+            + ' ' 
+            + lang(43) 
+            + '</div>'
+        );
+        errorStatus = true;
+        $('#' + fieldname).css('background', '#FFF9C5');
+    }
+    else
+    if(phone_expression != false)
+    {
+        var result = phone_expression.test(data_string);
+        if(result == false)
+        {
+            $('#' + fieldname + "-status").html(
+                '<div class=error_message id=' + fieldname + '_di ><i class="fa fa-info-circle text-red-md" aria-hidden="true"></i> ' 
+                + lang(41) 
+                + ' '
+                + message_item
+                + '</div>'
+            );
+            errorStatus = true;
+            $('#' + fieldname).css('background', '#FFF9C5');
+        }
+    }
+    else
+    {
+        $('#' + fieldname).css('background', '#fff');
+    }
+}
+ 
+function checkEmail(fieldname, message_item, email_expression)
+{
+    var data_string = $('#' + fieldname).val();
+    if(data_string.length == 0)
+    {
+        $('#' + fieldname + "-status").html(
+            '<div class=error_message id=' + fieldname + '_di ><i class="fa fa-info-circle text-red-md" aria-hidden="true"></i> ' 
+            + lang(42) 
+            + ' '
+            + message_item
+            + ' ' 
+            + lang(43) 
+            + '</div>'
+        );
+        errorStatus = true;
+        $('#' + fieldname).css('background', '#FFF9C5');
+    }
+    else
+    if(email_expression != false)
+    {
+        var result = email_expression.test(data_string);
+        if(result == false)
+        {
+            $('#' + fieldname + "-status").html(
+                '<div class=error_message id=' + fieldname + '_di ><i class="fa fa-info-circle text-red-md" aria-hidden="true"></i> ' 
+                + lang(41) 
+                + ' '
+                + message_item
+                + '</div>'
+            );
+            errorStatus = true;
+            $('#' + fieldname).css('background', '#FFF9C5');
+        }
+    }
+    else
+    {
+        $('#' + fieldname).css('background', '#fff');
+    }
+}
+ 
