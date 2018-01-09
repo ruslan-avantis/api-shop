bb1aaae8187d94c85f60d1598828b303ef935908
avantis <uaruscom@gmail.com>
2018-01-09 21:19:58 +0200

    Add files via upload

--- install.php
+++ install.php
@@ -16,6 +16,8 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
  
+$site = '<!DOCTYPE html><html lang="ru"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><title>API Shop - Installation</title><link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet"><link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" /><link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"><style>body,html{width:100%;height:100%}body{font-family:"Source Sans Pro"}.btn-xl{padding:1.25rem 2.5rem}.content-section{padding-top:7.5rem;padding-bottom:7.5rem}h1,h2,h3,h4,h5,h6{font-weight:700}.masthead{min-height:30rem;position:relative;display:table;width:100%;height:auto;padding-top:8rem;padding-bottom:8rem;background:linear-gradient(90deg,rgba(255,255,255,.1) 0,rgba(255,255,255,.1) 100%),url(https://blackrockdigital.github.io/startbootstrap-stylish-portfolio/img/bg-masthead.jpg);background-position:center center;background-repeat:no-repeat;background-size:cover}.masthead h1{font-size:4rem;margin:0;padding:0}@media (min-width:992px){.masthead{height:100vh}.masthead h1{font-size:5.5rem}}a{color:#1d809f}a:active,a:focus,a:hover{color:#155d74}.btn-primary{background-color:#1d809f!important;border-color:#1d809f!important;color:#fff!important}.btn-primary:active,.btn-primary:focus,.btn-primary:hover{background-color:#155d74!important;border-color:#155d74!important}.btn{box-shadow:0 3px 3px 0 rgba(0,0,0,.1);font-weight:700}.bg-primary{background-color:#1d809f!important}.text-primary{color:#1d809f!important}</style></head><body><header class="masthead d-flex"><div class="container text-center my-auto"><h1 class="mb-1">API Shop готов к работе !</h1><h3 class="mb-5"><em>Для загрузки дополнительного ПО, перейдите на главную</em></h3><a class="btn btn-primary btn-xl" href="/">На главную</a></div><div class="overlay"></div></header>';
+ 
 $file = __DIR__ . "/api-shop.zip";
  
 if (!file_exists($file)) {
@@ -33,7 +35,7 @@ if ($zip->open($file) === true) {
   $res = $zip->extractDirTo($dest, $dir);
   $zip->close();
   if ($res === true) {
-    echo 'Все ок ! <a href="/">Перейти на главную</a>';
+    echo $site;
   } else {
     echo 'При извлечении возникли ошибки';
   }
