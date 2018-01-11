825c3a73cedac118f1bfde91b915f3582630ccec
avantis <uaruscom@gmail.com>
2018-01-11 03:09:15 +0200

    Add files via upload

---
+++ themes/templates/install/install.js
@@ -0,0 +1,4 @@
+function Load(){
+    $("#loader-div").html("<div class=\'loader-fon\'><div class=\'loader-wrapper\'><div class=\'loader\'></div></div></div>");
+	$("#loader-link").html("<div class=\'load\'>Загружаю файлы ...</div>");
+}
