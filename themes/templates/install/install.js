907d051d9824621a3b2f645c664800476c8ea54d
avantis <uaruscom@gmail.com>
2018-01-12 18:13:18 +0200

    Add files via upload

--- themes/templates/install/install.js
+++ themes/templates/install/install.js
@@ -2,41 +2,8 @@ function Load() {
     $("#loader-div").html("<div class=\'loader-fon\'><div class=\'loader-wrapper\'><div class=\'loader\'></div></div></div>");
     $("#loader-link").html("<div class=\'load\'>Загружаю файлы ...</div>");
 }
-function checkStore(id) {
-    var csrf = $("#csrf").val()
-    $.post("/install-store", {id: id, csrf: csrf}, function (response) {
-        var data = JSON && JSON.parse(response) || $.parseJSON(response)
-        if(data.status == 200)
-        {
-            window.location.reload()
-        } else if(data.status == 400) {
-            OneNotify(data.title, data.text, data.color)
-        }
-    }),"json"
-}
-
-function checkTemplate(id) {
-    var csrf = $("#csrf").val()
-    var host = $("#host").val()
-    var uri = $("#uri-" + id).val()
-    var dir = $("#dir-" + id).val()
-    $.post("/install-template", {id: id, csrf: csrf, host: host, uri: uri, dir: dir}, function (response) {
-        var data = JSON && JSON.parse(response) || $.parseJSON(response)
-        if(data.status == 200)
-        {
-            // Сохраняем массив с локализацией у юзера
-            try {
-                window.location.reload()
-            } catch(e){
-                window.location.reload()
-            }
-        } else if(data.status == 400) {
-            OneNotify(data.title, data.text, data.color)
-        }
-    }),"json"
-}
-
-function checkInSeller() {
+ 
+function registerInSeller() {
     $('.error_message').remove()
     checkPhone('phone', lang(200), false)
     checkPassword('password', lang(201), password_expression)
@@ -48,12 +15,13 @@ function checkInSeller() {
     var password = $("#password").val()
     var iname = $("#iname").val()
     var fname = $("#fname").val()
+    var host = $("#host").val()
     var csrf = $("#csrf").val()
     setDb('phone', phone)
     setDb('email', email)
     setDb('iname', iname)
     setDb('fname', fname)
-    $.post("/install-in-seller", {email: email, phone: phone, password: password, iname: iname, fname: fname, csrf: csrf}, function (response) {
+    $.post("/register-in-seller", {email: email, phone: phone, password: password, iname: iname, fname: fname, host: host, csrf: csrf}, function (response) {
         var data = $.parseJSON(response)
         if(data.status == 200) {
             window.location.reload()
@@ -65,7 +33,50 @@ function checkInSeller() {
     ),"json"
 }
  
-function checkApiKey() {
+function startShop() {
+    var csrf = $("#csrf").val()
+    $.post("/start-shop", {csrf: csrf}, function (response) {
+        var data = $.parseJSON(response)
+        if(data.status == 200) {
+            window.location.reload()
+        }
+        else if(data.status == 400) {
+            OneNotify(data.title, data.text, data.color)
+        }
+    }
+    ),"json"
+}
+ 
+function installStore(id) {
+    var csrf = $("#csrf").val()
+    $.post("/install-store", {id: id, csrf: csrf}, function (response) {
+        var data = JSON && JSON.parse(response) || $.parseJSON(response)
+        if(data.status == 200)
+        {
+            window.location.reload()
+        } else if(data.status == 400) {
+            OneNotify(data.title, data.text, data.color)
+        }
+    }),"json"
+}
+ 
+function installTemplate(id) {
+    var csrf = $("#csrf").val()
+    var host = $("#host").val()
+    var uri = $("#uri-" + id).val()
+    var dir = $("#dir-" + id).val()
+    $.post("/install-template", {id: id, csrf: csrf, host: host, uri: uri, dir: dir}, function (response) {
+        var data = JSON && JSON.parse(response) || $.parseJSON(response)
+        if(data.status == 200)
+        {
+            window.location.reload()
+        } else if(data.status == 400) {
+            OneNotify(data.title, data.text, data.color)
+        }
+    }),"json"
+}
+ 
+function installApiKey() {
     $('.error_message').remove()
     checkData('public_key', 'public_key', false)
     var public_key = $("#public_key").val()
@@ -81,8 +92,8 @@ function checkApiKey() {
     }
     ),"json"
 }
-
-function checkKey() {
+ 
+function installKey() {
     var csrf = $("#csrf").val()
     $.post("/install-key", {csrf: csrf}, function (response) {
         var data = $.parseJSON(response)
@@ -95,8 +106,8 @@ function checkKey() {
     }
     ),"json"
 }
-
-function checkNoKey() {
+ 
+function installNoKey() {
     var csrf = $("#csrf").val()
     $.post("/install-no-key", {csrf: csrf}, function (response) {
         var data = $.parseJSON(response)
