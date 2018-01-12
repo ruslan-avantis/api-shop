8269feea7c709a838f439ea7b394a50e9ebe757f
avantis <uaruscom@gmail.com>
2018-01-12 14:39:17 +0200

    Add files via upload

--- app/config/settings.php
+++ app/config/settings.php
@@ -10,44 +10,16 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
+ 
 namespace ApiShop\Config;
-
+ 
 use Defuse\Crypto\Key;
-
+ 
 class Settings {
-    
+ 
     public static function get() {
  
     $config = array();
- 
-    $config['settings']['site']['title'] = "API Shop";
-    $config['settings']['site']['description'] = "Работает через RESTful API";
-    $copyYear = 2017; // Set your website start date
-    $curYear = date('Y'); // Keeps the second year updated
-    $config['settings']['site']['copyright']['date'] = $copyYear . (($copyYear != $curYear) ? '-' . $curYear : '');
-    $config['settings']['site']['copyright']['text'] = "API Shop";
-    $config['settings']['site']['currency_id'] = 1;
-
-    // Если сайт работает через https должно быть true
-    $config['settings']['site']['cookie_httponly'] = false;
- 
-    // Путь к ключам шифрования
-    $key = __DIR__ . "/key";
-    if (!file_exists($key)) {
-        mkdir($key, 0777, true);
-    }
- 
-    // Проверяем наличие public_key
-    $public_key = $key."/public_key.txt";
- 
-    $config["settings"]["install"]["key"] = $public_key;
- 
-    if (file_exists($public_key)) {
-        $config["settings"]["install"]["public_key"] = file_get_contents($public_key, true);
-    } else {
-	    $config["settings"]["install"]["public_key"] = null;
-    }
  
     // Путь к папке шаблонов
     $config["settings"]["themes"]["dir"] = __DIR__ . "/../../themes";
@@ -57,87 +29,73 @@ class Settings {
     // Если работает через api будет брать название шаблона с конфигурации api
     $config["settings"]["themes"]["template"] = "mini-mo";
  
-    // Папка куда будет кешироваться Slim\Views\Twig
-    $config["settings"]["cache"] =  __DIR__ . "/../_cache/";
-    // Коды ответов и ошибок
-    $config["settings"]['http-codes'] = "https://github.com/pllano/APIS-2018/tree/master/http-codes/";
- 
-    // Конфигурация Slim
-    $config["settings"]["dir"] = "config";
-    $config["settings"]["displayErrorDetails"] = false;
-    $config["settings"]["addContentLengthHeader"] = false;
-    $config["settings"]["determineRouteBeforeAppMiddleware"] = true;
-    $config["settings"]["cookies.httponly"] = true;
-    $config["settings"]["phpSettings.session.cookie_httponly"] = true;
-    $config["settings"]["rebodys.session.cookie_httponly"] = true;
-    $config["settings"]["debug"] = false;
- 
-    // Конфигурация session
-    $config["settings"]["session"]["name"] = "_session";
-    $config["settings"]["session"]["lifetime"] = 48;
-    $config["settings"]["session"]["path"] = "/";
-    $config["settings"]["session"]["domain"] = "";
-    $config["settings"]["session"]["secure"] = false;
-    $config["settings"]["session"]["httponly"] = true;
- 
-    // Set session cookie path, domain and secure automatically
-    $config["settings"]["session"]["cookie_autoset"] = true;
-    // Path where session files are stored, PHP"s default path will be used if set null
-    $config["settings"]["session"]["save_path"] = null;
-    // Session cache limiter
-    $config["settings"]["session"]["cache_limiter"] = "nocache";
-    // Extend session lifetime after each user activity
-    $config["settings"]["session"]["autorefresh"] = true;
-    // Encrypt session data if string is set
-    $config["settings"]["session"]["encryption_key"] = null;
-    // Session namespace
-    $config["settings"]["session"]["namespace"] = "_session";
- 
-    // Папка куда будут писатся логи Monolog
-    $config["settings"]["logger"]["path"]   = isset($_ENV["docker"]) ? "php://stdout" : __DIR__ . "/../_logs/app.log";
-    $config["settings"]["logger"]["name"]   = "slim-app";
-    $config["settings"]["logger"]["level"] = \Monolog\Logger::DEBUG;
- 
-    $key_session = $key."/session.txt";
-    $key_cookie = $key."/cookie.txt";
-    $key_token = $key."/token.txt";
-    $key_password = $key."/password.txt";
-    $key_user = $key."/user.txt";
-    $key_card = $key."/card.txt";
-    $key_db = $key."/db.txt";
- 
-    // Устанавливаем ключи шифрования
-    if (!file_exists($key_session)) {
-        file_put_contents($key_session, (Key::createNewRandomKey())->saveToAsciiSafeString());
-    }
-    if (!file_exists($key_cookie)) {
-        file_put_contents($key_cookie, (Key::createNewRandomKey())->saveToAsciiSafeString());
-    }
-    if (!file_exists($key_token)) {
-        file_put_contents($key_token, (Key::createNewRandomKey())->saveToAsciiSafeString());
-    }
-    if (!file_exists($key_password)) {
-        file_put_contents($key_password, (Key::createNewRandomKey())->saveToAsciiSafeString());
-    }
-    if (!file_exists($key_user)) {
-        file_put_contents($key_user, (Key::createNewRandomKey())->saveToAsciiSafeString());
-    }
-    if (!file_exists($key_card)) {
-        file_put_contents($key_card, (Key::createNewRandomKey())->saveToAsciiSafeString());
-    }
-    if (!file_exists($key_db)) {
-        file_put_contents($key_db, (Key::createNewRandomKey())->saveToAsciiSafeString());
+    // Путь к ключам шифрования
+    $key = __DIR__ . "/key";
+    // Создаем директорию если ее еще нет.
+    if (!file_exists($key)) {
+        mkdir($key, 0777, true);
     }
  
-    $config["key"]["session"] = Key::loadFromAsciiSafeString(file_get_contents($key_session, true));
-    $config["key"]["token"] = Key::loadFromAsciiSafeString(file_get_contents($key_token, true));
-    $config["key"]["cookie"] = Key::loadFromAsciiSafeString(file_get_contents($key_cookie, true));
-    $config["key"]["password"] = Key::loadFromAsciiSafeString(file_get_contents($key_password, true));
-    $config["key"]["user"] = Key::loadFromAsciiSafeString(file_get_contents($key_user, true));
-    $config["key"]["card"] = Key::loadFromAsciiSafeString(file_get_contents($key_card, true));
-    // Динамический ключ шифрования для ajax
-    $config["key"]["ajax"] = (Key::createNewRandomKey())->saveToAsciiSafeString();
-
+    // Директория хранения файлов базы данных json
+    $config["db"]["json"]["dir"] = __DIR__ . "/_json_db_/";
+ 
+    // Если директории нет создать.
+    if (!file_exists($config["db"]["json"]["dir"])) {
+        mkdir($config["db"]["json"]["dir"], 0777, true);
+    }
+ 
+    // Резервное хранилище для public_key
+    $public_key_file = $key."/public_key.txt";
+    // public_key для активации сайта при установке
+    $config["settings"]["install"]["file"] = $public_key_file;
+    // Длина ключа public_key - колличество символов
+    $config["settings"]["install"]["strlen"] = 64;
+ 
+    // Получаем public_key из базы, если база уже создана
+    if (file_exists($config["db"]["json"]["dir"]."db.data.json")) {
+        $json = json_decode(file_get_contents($config["db"]["json"]["dir"]."db.data.json"), true);
+        if (isset($json["0"]["public_key"])) {
+             $public_key = $json["0"]["public_key"];
+        } else {
+             $public_key = null;
+        }
+    } else {
+        // Проверяем резервное хранилище public_key
+        if (file_exists($public_key_file)) {
+            $public_key = file_get_contents($public_key_file, true);
+        } else {
+            $public_key = null;
+        }
+    }
+    
+    if(strlen($public_key) != $config["settings"]["install"]["strlen"]) {
+        $public_key = null;
+    }
+ 
+    // Статус активации сайта null или любое другое значение
+    $config["settings"]["install"]["status"] = $public_key;
+ 
+    // Кеширование запросов
+    $config["db"]["json"]["cached"] = false; // true|false
+    // Время жизни кеша
+    $config["db"]["json"]["cache_lifetime"] = 60;
+    // Очередь на запись
+    $config["db"]["json"]["temp"] = false; // false|true
+    // Работает через API
+    $config["db"]["json"]["api"] = false; // false|true
+    // Шифруем базу
+    $config["db"]["json"]["crypt"] = false; // false|true
+ 
+    // Настройки подключения к jsondb через API
+    // URL API jsondb
+    $config["db"]["jsonapi"]["url"] = "";
+    // Доступные методы аутентификации: null, CryptoAuth, QueryKeyAuth, HttpTokenAuth, LoginPasswordAuth
+    $config["db"]["jsonapi"]["auth"] = null;
+    // Публичный ключ аутентификации
+    $config["db"]["jsonapi"]["public_key"] = "";
+    // Приватный ключ шифрования
+    $config["db"]["jsonapi"]["private_key"] = "";
+ 
     // Глобальные установки баз данных
     // Доступные значения: api, json, jsonapi, mysql, elasticsearch
     // Название основной базы данных. По умолчанию api
@@ -154,36 +112,15 @@ class Settings {
     $config["db"]["synchronize"] = false; // false|true
     // Коды ответов и ошибок
     $config["db"]['http-codes'] = "https://github.com/pllano/APIS-2018/tree/master/http-codes/";
+ 
+    $key_db = $key."/db.txt";
+ 
+    if (!file_exists($key_db)) {
+        file_put_contents($key_db, (Key::createNewRandomKey())->saveToAsciiSafeString());
+    }
+ 
     // Ключ шифрования в базах данных. Отдаем в чистом виде.
     $config["db"]["key"] = file_get_contents($key_db, true);
-
-    // Настройки подключения к jsondb напрямую
-    // Директория для хранения файлов json базы данных.
-    $config["db"]["json"]["dir"] = __DIR__ . "/_json_db_/";
-    // Если директории нет создать.
-    if (!file_exists($config["db"]["json"]["dir"])) {
-        mkdir($config["db"]["json"]["dir"], 0777, true);
-    }
-    // Кеширование запросов
-    $config["db"]["json"]["cached"] = false; // true|false
-    // Время жизни кеша
-    $config["db"]["json"]["cache_lifetime"] = 60;
-    // Очередь на запись
-    $config["db"]["json"]["temp"] = false; // false|true
-    // Работает через API
-    $config["db"]["json"]["api"] = false; // false|true
-    // Шифруем базу
-    $config["db"]["json"]["crypt"] = false; // false|true
-
-    // Настройки подключения к jsondb через API
-    // URL API jsondb
-    $config["db"]["jsonapi"]["url"] = "https://xti.com.ua/json-db/";
-    // Доступные методы аутентификации: null, CryptoAuth, QueryKeyAuth, HttpTokenAuth, LoginPasswordAuth
-    $config["db"]["jsonapi"]["auth"] = null;
-    // Публичный ключ аутентификации
-    $config["db"]["jsonapi"]["public_key"] = "";
-    // Приватный ключ шифрования
-    $config["db"]["jsonapi"]["private_key"] = "";
  
     // Если работает через API будет брать часть конфигурации из api
     $config["db"]["api"]["config"] = true; // true|false
@@ -191,17 +128,17 @@ class Settings {
     $config["db"]["api"]["url"] = "https://ua.pllano.com/api/v1/json/";
     // Доступные методы аутентификации: CryptoAuth, QueryKeyAuth, HttpTokenAuth, LoginPasswordAuth
     $config["db"]["api"]["auth"] = "QueryKeyAuth";
-   // Приватный ключ шифрования
+    // Приватный ключ шифрования
     $config["db"]["api"]["private_key"] = "";
     // Публичный ключ аутентификации
-	// По умолчанию генерируется при установке сайта
-	// Можете заменить на статический ключ доступа
-	if (file_exists($public_key)) {
-        $config["db"]["api"]["public_key"] = file_get_contents($public_key, true);
-	} else {
-	    $config["db"]["api"]["public_key"] = null;
-	}
-	// $config["db"]["api"]["public_key"] = "";
+    // По умолчанию генерируется при установке сайта
+    // Можете заменить на статический ключ доступа
+    if ($public_key != null) {
+        $config["db"]["api"]["public_key"] = $public_key;
+    } else {
+        $config["db"]["api"]["public_key"] = null;
+    }
+    // $config["db"]["api"]["public_key"] = "";
  
     // Настройки подключения к базе MySQL
     $config["db"]["mysql"]["host"] = "localhost";
@@ -226,23 +163,111 @@ class Settings {
     $config["db"]["elasticsearch"]["user"] = "elastic";
     $config["db"]["elasticsearch"]["password"] = "elastic_password";
  
+    // Папка куда будет кешироваться Slim\Views\Twig
+    $config["settings"]["cache"] =  __DIR__ . "/../_cache/";
+    // Коды ответов и ошибок
+    $config["settings"]['http-codes'] = "https://github.com/pllano/APIS-2018/tree/master/http-codes/";
+ 
+    // Конфигурация Slim
+    $config["settings"]["dir"] = "config";
+    $config["settings"]["displayErrorDetails"] = true;
+    $config["settings"]["addContentLengthHeader"] = false;
+    $config["settings"]["determineRouteBeforeAppMiddleware"] = true;
+    $config["settings"]["cookies.httponly"] = true;
+    $config["settings"]["phpSettings.session.cookie_httponly"] = true;
+    $config["settings"]["rebodys.session.cookie_httponly"] = true;
+    $config["settings"]["debug"] = true;
+ 
+    $config['settings']['site']['title'] = "API Shop";
+    $config['settings']['site']['description'] = "Работает через RESTful API";
+    $copyYear = 2017; // Set your website start date
+    $curYear = date('Y'); // Keeps the second year updated
+    $config['settings']['site']['copyright']['date'] = $copyYear . (($copyYear != $curYear) ? '-' . $curYear : '');
+    $config['settings']['site']['copyright']['text'] = "API Shop";
+    $config['settings']['site']['currency_id'] = 1;
+
+    // Если сайт работает через https должно быть true
+    $config['settings']['site']['cookie_httponly'] = false;
+ 
+    // Конфигурация session
+    $config["settings"]["session"]["name"] = "_session";
+    $config["settings"]["session"]["lifetime"] = 48;
+    $config["settings"]["session"]["path"] = "/";
+    $config["settings"]["session"]["domain"] = "";
+    $config["settings"]["session"]["secure"] = false;
+    $config["settings"]["session"]["httponly"] = true;
+    // Set session cookie path, domain and secure automatically
+    $config["settings"]["session"]["cookie_autoset"] = true;
+    // Path where session files are stored, PHP"s default path will be used if set null
+    $config["settings"]["session"]["save_path"] = null;
+    // Session cache limiter
+    $config["settings"]["session"]["cache_limiter"] = "nocache";
+    // Extend session lifetime after each user activity
+    $config["settings"]["session"]["autorefresh"] = true;
+    // Encrypt session data if string is set
+    $config["settings"]["session"]["encryption_key"] = null;
+    // Session namespace
+    $config["settings"]["session"]["namespace"] = "_session";
+ 
+    // Папка куда будут писатся логи Monolog
+    $config["settings"]["logger"]["path"]   = isset($_ENV["docker"]) ? "php://stdout" : __DIR__ . "/../_logs/app.log";
+    $config["settings"]["logger"]["name"]   = "slim-app";
+    $config["settings"]["logger"]["level"] = \Monolog\Logger::DEBUG;
+ 
+    // Директория где хранятся ключи шифрования
+    $key_session = $key."/session.txt";
+    $key_cookie = $key."/cookie.txt";
+    $key_token = $key."/token.txt";
+    $key_password = $key."/password.txt";
+    $key_user = $key."/user.txt";
+    $key_card = $key."/card.txt";
+ 
+    // Генерируем ключи шифрования, если их нет
+    if (!file_exists($key_session)) {
+        file_put_contents($key_session, (Key::createNewRandomKey())->saveToAsciiSafeString());
+    }
+    if (!file_exists($key_cookie)) {
+        file_put_contents($key_cookie, (Key::createNewRandomKey())->saveToAsciiSafeString());
+    }
+    if (!file_exists($key_token)) {
+        file_put_contents($key_token, (Key::createNewRandomKey())->saveToAsciiSafeString());
+    }
+    if (!file_exists($key_password)) {
+        file_put_contents($key_password, (Key::createNewRandomKey())->saveToAsciiSafeString());
+    }
+    if (!file_exists($key_user)) {
+        file_put_contents($key_user, (Key::createNewRandomKey())->saveToAsciiSafeString());
+    }
+    if (!file_exists($key_card)) {
+        file_put_contents($key_card, (Key::createNewRandomKey())->saveToAsciiSafeString());
+    }
+ 
+    $config["key"]["session"] = Key::loadFromAsciiSafeString(file_get_contents($key_session, true));
+    $config["key"]["token"] = Key::loadFromAsciiSafeString(file_get_contents($key_token, true));
+    $config["key"]["cookie"] = Key::loadFromAsciiSafeString(file_get_contents($key_cookie, true));
+    $config["key"]["password"] = Key::loadFromAsciiSafeString(file_get_contents($key_password, true));
+    $config["key"]["user"] = Key::loadFromAsciiSafeString(file_get_contents($key_user, true));
+    $config["key"]["card"] = Key::loadFromAsciiSafeString(file_get_contents($key_card, true));
+    // Динамический ключ шифрования для ajax
+    $config["key"]["ajax"] = (Key::createNewRandomKey())->saveToAsciiSafeString();
+ 
     // API Shop позволяет одновременно работать с любым количеством баз данных
     // Название базы данных для каждого ресурса. По умолчанию api
- 
-    // Хранилище для ресурса site
-    $config["db"]["resource"]["site"]["db"] = "api"; // +
-    // Синхронизировать ресурс site или нет. По умолчанию false
-    $config["db"]["resource"]["site"]["synchronize"] = false;
  
     // Хранилище для ресурса install_stores_list
     $config["db"]["resource"]["install_stores_list"]["db"] = "json"; // +
     // Синхронизировать ресурс install_stores_list или нет. По умолчанию false
     $config["db"]["resource"]["install_stores_list"]["synchronize"] = false;
  
-    // Хранилище для ресурса install_templates
-    $config["db"]["resource"]["install_templates"]["db"] = "json"; // +
-    // Синхронизировать ресурс install_templates или нет. По умолчанию false
-    $config["db"]["resource"]["install_templates"]["synchronize"] = false;
+    // Хранилище для ресурса install_templates_list
+    $config["db"]["resource"]["install_templates_list"]["db"] = "json"; // +
+    // Синхронизировать ресурс install_templates_list или нет. По умолчанию false
+    $config["db"]["resource"]["install_templates_list"]["synchronize"] = false;
+ 
+    // Хранилище для ресурса site
+    $config["db"]["resource"]["site"]["db"] = "api"; // +
+    // Синхронизировать ресурс site или нет. По умолчанию false
+    $config["db"]["resource"]["site"]["synchronize"] = false;
  
     // Хранилище для ресурса price
     $config["db"]["resource"]["price"]["db"] = "api";
@@ -348,6 +373,26 @@ class Settings {
     $config["db"]["resource"]["article_category"]["db"] = "json";
     // Синхронизировать ресурс article_category или нет. По умолчанию false
     $config["db"]["resource"]["article_category"]["synchronize"] = false;
+    
+    // Хранилище для ресурса article_category
+    $config["db"]["resource"]["article_category"]["db"] = "json";
+    // Синхронизировать ресурс article_category или нет. По умолчанию false
+    $config["db"]["resource"]["article_category"]["synchronize"] = false;
+ 
+    // Хранилище для ресурса queue
+    $config["db"]["resource"]["queue"]["db"] = "json";
+    // Синхронизировать ресурс queue или нет. По умолчанию false
+    $config["db"]["resource"]["queue"]["synchronize"] = false;
+ 
+    // Хранилище для ресурса cached
+    $config["db"]["resource"]["cached"]["db"] = "json";
+    // Синхронизировать ресурс cached или нет. По умолчанию false
+    $config["db"]["resource"]["cached"]["synchronize"] = false;
+ 
+    // Хранилище для ресурса db
+    $config["db"]["resource"]["db"]["db"] = "json";
+    // Синхронизировать ресурс db или нет. По умолчанию false
+    $config["db"]["resource"]["db"]["synchronize"] = false;
  
     return $config;
  
