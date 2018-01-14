8999e1de5d2cc5521964868239a3edcdcbc65ca9
avantis <uaruscom@gmail.com>
2018-01-14 20:16:22 +0200

    Add files via upload

--- vendor/pllano/api-shop/Model/SessionUser.php
+++ vendor/pllano/api-shop/Model/SessionUser.php
@@ -5,32 +5,9 @@ namespace ApiShop\Model;
 use Defuse\Crypto\Crypto;
 use Sinergi\BrowserDetector\Language as Langs;
 use ApiShop\Config\Settings;
-use ApiShop\Resources\User;
 use Adbar\Session;
  
 class SessionUser {
- 
-    // Контролируем наличие всех необходимых данных
-    // Если пользователь авторизовался или регестрировался на другом ресурсе
-    public function checking()
-    {
-        $config = (new Settings())->get();
-        // Подключаем сессию
-        $session = new Session($config['settings']['session']['name']);
-        if (!$session->user_id && !$session->iname && !$session->fname && !$session->phone && !$session->email) {
-            // Читаем ключи
-            $session_key = $config['key']['session'];
-            // Получаем данные пользователя по cookie
-			$data = (new User())->getUserCode();
-            if ($data != null) {
-			    $session->user_id = $data["items"]["0"]["item"]["user_id"];
-                $session->iname = Crypto::encrypt($data["items"]["0"]["item"]["iname"], $session_key);
-                $session->fname = Crypto::encrypt($data["items"]["0"]["item"]["fname"], $session_key);
-                $session->phone = Crypto::encrypt($data["items"]["0"]["item"]["phone"], $session_key);
-                $session->email = Crypto::encrypt($data["items"]["0"]["item"]["email"], $session_key);
-            }
-		}
-    }
  
     // Получаем данные из session
     public function get() {
@@ -40,6 +17,7 @@ class SessionUser {
 		$langs = new Langs();
  
         // Получаем массив данных из таблицы language на языке из $session->language
+		$lang = "en";
         if (isset($session->language)) {
             $lang = $session->language;
         } elseif ($langs->getLanguage()) {
@@ -56,7 +34,10 @@ class SessionUser {
         		$session_key = $config['key']['session'];
             	// Формируем массив данных сессии который отдаем шаблонизатору
             	$response = array();
-				if ($lang) {$response['language'] = $lang;}
+				$response['language'] = $lang;
+				$response["authorize"] = $session->authorize;
+				if (isset($session->role_id)) {$response["role_id"] = $session->role_id;}
+				if (isset($session->user_id)) {$response["user_id"] = $session->user_id;}
             	if (isset($session->iname)) {$response["iname"] = Crypto::decrypt($session->iname, $session_key);}
             	if (isset($session->fname)) {$response["fname"] = Crypto::decrypt($session->fname, $session_key);}
             	if (isset($session->phone)) {$response['phone'] = Crypto::decrypt($session->phone, $session_key);}
@@ -65,15 +46,17 @@ class SessionUser {
             	return $response;
         	} else {
 		    	$response = array();
-				if (isset($session->language)) {$response['language'] = $session->language;}
+				$response['language'] = $lang;
+				$response['authorize'] = $session->authorize;
 				return $response;
 			}
 		} else {
 		    	$response = array();
-				if (isset($session->language)) {$response['language'] = $session->language;}
+				$response['language'] = $lang;
+				$response['authorize'] = $session->authorize;
 				return $response;
 		}
     }
-
+ 
 }
  
--- vendor/pllano/api-shop/Resources/User.php
+++ vendor/pllano/api-shop/Resources/User.php
@@ -32,95 +32,100 @@ class User {
         $config = (new Settings())->get();
         // Подключаем сессию \Adbar\Session
         $session = new Session($config['settings']['session']['name']);
-        // Получаем массив данных из таблицы language на языке из $session->language
-        $langs = new Langs();
-        // Перезаписываем язык в сессии
-        if (isset($session->language)) {
-            $lang = $session->language;
-        } elseif ($langs->getLanguage()) {
-            $lang = $langs->getLanguage();
-        } else {
-            $lang = $site_config["language"];
-        }
-        $session->language = $lang;
-        // Читаем ключи
+		// Читаем ключи
         $session_key = $config['key']['session'];
-        $cookie_key = $config['key']['cookie'];
+		$cookie_key = $config['key']['cookie'];
+ 
         // Читаем печеньку у юзера в браузере
-        $read_code = isset($_COOKIE[$config['settings']['session']['name']]) ? $_COOKIE[$config['settings']['session']['name']] : null;
-        // Если печеньки нет создаем новую
-        if ($read_code) {
+        $identificator = isset($_COOKIE[$config['settings']['session']['name']]) ? $_COOKIE[$config['settings']['session']['name']] : null;
+ 
+        if ($identificator != null) {
             try {
-                $cookie = Crypto::decrypt($_COOKIE[$config['settings']['session']['name']], $cookie_key);
-                $identificator = $_COOKIE[$config['settings']['session']['name']];
+                $cookie = Crypto::decrypt($identificator, $cookie_key);
             } catch (CryptoEx $ex) {
-                $identificator = null;
+                $cookie = null;
+            }
+ 
+            if ($cookie != null) {
+                // Ресурс (таблица) к которому обращаемся
+                $resource = "user";
+                // Отдаем роутеру RouterDb конфигурацию.
+                $router = new Router($config);
+                // Получаем название базы для указанного ресурса
+                $name_db = $router->ping($resource);
+                // Подключаемся к базе
+                $db = new Db($name_db, $config);
+                // Отправляем запрос и получаем данные
+                $response = $db->get($resource, ["cookie" => $cookie, "state" => 1]);
+ 
+				//print("<br>");
+				//print_r($response);
+				if (isset($response["headers"]["code"])) {
+				    if ($response["headers"]["code"] == 200 || $response["headers"]["code"] == "200") {
+ 
+				        if(is_object($response["body"]["items"]["0"]["item"])) {
+				            $user = (array)$response["body"]["items"]["0"]["item"];
+				        } elseif (is_array($response["body"]["items"]["0"]["item"])) {
+				            $user = $response["body"]["items"]["0"]["item"];
+				        }
+ 
+				        if ($user["state"] == 1) {
+				            $session->authorize = 1;
+				            $session->role_id = $user["role_id"];
+				            $session->user_id = $user["id"];
+				            $session->iname = Crypto::encrypt($user["iname"], $session_key);
+				            $session->fname = Crypto::encrypt($user["fname"], $session_key);
+				            $session->phone = Crypto::encrypt($user["phone"], $session_key);
+				            $session->email = Crypto::encrypt($user["email"], $session_key);
+				        } else {
+				            $session->authorize = null;
+				            $session->role_id = null;
+				            $session->user_id = null;
+				            unset($session->authorize); // удаляем authorize
+				            unset($session->role_id); // удаляем role_id
+				            unset($session->user_id); // удаляем role_id
+				            $session->destroy();
+				            $session->clear();
+				        }
+				    }
+				} else {
+				    $session->authorize = null;
+				    $session->role_id = null;
+				    $session->user_id = null;
+				    unset($session->authorize); // удаляем authorize
+				    unset($session->role_id); // удаляем role_id
+				    unset($session->user_id); // удаляем role_id
+			    }
             }
         } else {
-            $identificator = null;
-        }
-        // Если cookie нет создаем новую
-        if ($identificator === null) {
-            // Чистим сессию на всякий случай
-            $session->clear();
-            // Подключаем утилиту
-            $utility = new Utility();
-            // Генерируем identificator
-            $identificator = Crypto::encrypt($utility->random_token(), $cookie_key);
-            // Записываем пользователю новый cookie
-            $domain = ($_SERVER['HTTP_HOST'] != 'localhost') ? $_SERVER['HTTP_HOST'] : false;
-            if ($config['settings']['site']['cookie_httponly'] === true) {
-                setcookie($config['settings']['session']['name'], $identificator, time()+60*60*24*365, '/', $domain, 1, true);
-            } else {
-                setcookie($config['settings']['session']['name'], $identificator, time()+60*60*24*365, '/', $domain);
-            }
-            // Пишем в сессию identificator cookie
-            $session->cookie = $identificator;
-        }
-        // Если cookie есть и дешифровка не дала ошибки
-        if ($identificator != null) {
-            // Пишем в сессию identificator cookie
-            $session->cookie = $identificator;
-            // cookie в расшифрованном виде для записи в базу
-            $cookie = Crypto::decrypt($identificator, $cookie_key);
-  
-            // Ресурс (таблица) к которому обращаемся
-            $resource = "user";
-            // Отдаем роутеру RouterDb конфигурацию.
-            $router = new Router($config);
-            // Получаем название базы для указанного ресурса
-            $name_db = $router->ping($resource);
-            // Подключаемся к базе
-            $db = new Db($name_db, $config);
-            // Отправляем запрос и получаем данные
-            $response = $db->get($resource, ["cookie" => $cookie]);
- 
-            // Если нашли пользователя в базе и получили его id
-            if ($response != null && isset($response["body"]["items"]['0']["item"]['user_id'])) {
- 
-                $user_id = $response["body"]["items"]['0']["item"]['user_id'];
-                $db->put($resource, ["cookie" => $cookie], $user_id);
- 
-                // Пишем данные из базы в сессию
-                $session->id = Crypto::encrypt($user_id, $session_key);
-                $session->user_id = Crypto::encrypt($user_id, $session_key);
-                //$session->cart_id = Crypto::encrypt($response["items"]["item"]['0']['cart_id'], $session_key);
-                $authorize = ($response["body"]["items"]['0']["item"]['authorized'] == '0000-00-00 00:00:00') ? 0 : 1;
-                // Записываем authorize в сессию
-                $session->authorize = $authorize;
-                // Если пользователь есть мы на всякий случай обновим данные в его сессии
-                $sessionUser = new SessionUser();
-                $sessionUser->checking();
-            } else {
-                // Если нет то обнуляем данные в сессии на всякий случай
-                $session->id = null;
-                $session->user_id = null;
-                //$session->cart_id = null;
-                $session->authorize = null;
+            // Если cookie нет создаем новую
+            if ($identificator === null) {
+                // Чистим сессию на всякий случай
+                $session->clear();
+                // Подключаем утилиту
+                $utility = new Utility();
+                // Генерируем identificator
+                $identificator = Crypto::encrypt($utility->random_token(), $cookie_key);
+                // Записываем пользователю новый cookie
+                $domain = ($_SERVER['HTTP_HOST'] != 'localhost') ? $_SERVER['HTTP_HOST'] : false;
+                if ($config['settings']['site']['cookie_httponly'] === true) {
+                    setcookie($config['settings']['session']['name'], $identificator, time()+60*60*24*365, '/', $domain, 1, true);
+                } else {
+                    setcookie($config['settings']['session']['name'], $identificator, time()+60*60*24*365, '/', $domain);
+                }
+                // Пишем в сессию identificator cookie
+                $session->cookie = $identificator;
             }
         }
+		
+        if (!isset($session->language)) {
+			$langs = new Langs();
+		    if ($langs->getLanguage()) {
+                $session->language = $langs->getLanguage();
+            }
+		}
     }
-
+ 
     // Авторизвация
     public function checkLogin($email, $phone, $password)
     {
@@ -151,82 +156,60 @@ class User {
         }
     }
  
-    // Получить данные пользователя по cookie
-    public function getUserCode()
+    // Обновляем cookie в базе
+    public function putUserCode($user_id)
     {
         // Получаем конфигурацию \ApiShop\Config\Settings
         $config = (new Settings())->get();
-        // Читаем ключи
-        $cookie_key = $config['key']['cookie'];
         // Подключаем сессию
         $session = new Session($config['settings']['session']['name']);
-        // Читаем печеньку у юзера в браузере
-        $session_code = isset($_COOKIE[$config['settings']['session']['name']]) ? $_COOKIE[$config['settings']['session']['name']] : null;
-        if ($session_code) {
-            try {
-                $cookie = Crypto::decrypt($session_code, $cookie_key);
-            } catch (CryptoEx $ex) {
-                $cookie = null;
-            }
-            if ($cookie != null) {
  
-                // Ресурс (таблица) к которому обращаемся
-                $resource = "user";
-                // Отдаем роутеру RouterDb конфигурацию.
-                $router = new Router($config);
-                // Получаем название базы для указанного ресурса
-                $name_db = $router->ping($resource);
-                // Подключаемся к базе
-                $db = new Db($name_db, $config);
-                // Отправляем запрос и получаем данные
-                $response = $db->get($resource, ["cookie" => $cookie]);
- 
-                if (isset($response["headers"]["code"])) {
-                    // Возвращаем ответ на запрос
-                    return $response;
-                } else {
-                    return null;
-                }
-            } else {
-                return null;
-            }
-        } else {
-            return null;
-        }
-    }
- 
-    // Обновляем cookie в базе
-    public function putUserCode($user_id, $cookie)
-    {
+        // Текущая дата
         $today = date("Y-m-d H:i:s");
-        // Получаем конфигурацию \ApiShop\Config\Settings
-        $config = (new Settings())->get();
-        // Читаем ключи шифрования
-        $cookie_key = $config['key']['cookie'];
-        $new_cookie = Crypto::encrypt($cookie, $cookie_key);
-        // Перезаписываем cookie у пользователя
-        $domain = ($_SERVER['HTTP_HOST'] != 'localhost') ? $_SERVER['HTTP_HOST'] : false;
-        if ($config['settings']['site']['cookie_httponly'] === true) {
-            setcookie($config['settings']['session']['name'], $new_cookie, time()+60*60*24*365, '/', $domain, 1, true);
-        } else {
-            setcookie($config['settings']['session']['name'], $new_cookie, time()+60*60*24*365, '/', $domain);
-        }
+        // Подключаем утилиту
+        $utility = new Utility();
+        // Генерируем новый cookie
+        $cookie = $utility->random_token();
+ 
         // Ресурс (таблица) к которому обращаемся
         $resource = "user";
         // Отдаем роутеру RouterDb конфигурацию.
         $router = new Router($config);
         // Получаем название базы для указанного ресурса
         $name_db = $router->ping($resource);
- 
         // Подключаемся к базе
         $db = new Db($name_db, $config);
         // Отправляем запрос и получаем данные
         $response = $db->put($resource, ["cookie" => $cookie, "authorized" => $today], $user_id);
  
+        // Если удалось обновить cookie в базе перезапишем везде
         if (isset($response["headers"]["code"])) {
-            // Если все ок возвращаем 1
-            return 1;
+            if ($response["headers"]["code"] == 202 || $response["headers"]["code"] == "202") {
+                // Читаем ключи шифрования
+                $cookie_key = $config['key']['cookie'];
+                // Шифруем cookie
+                $new_cookie = Crypto::encrypt($cookie, $cookie_key);
+ 
+                // Перезаписываем cookie в сессии
+                $session->cookie = $new_cookie;
+ 
+                // Перезаписываем cookie в базе
+                $domain = ($_SERVER['HTTP_HOST'] != 'localhost') ? $_SERVER['HTTP_HOST'] : false;
+                if ($config['settings']['site']['cookie_httponly'] === true) {
+                    setcookie($config['settings']['session']['name'], $new_cookie, time()+60*60*24*365, '/', $domain, 1, true);
+                } else {
+                    setcookie($config['settings']['session']['name'], $new_cookie, time()+60*60*24*365, '/', $domain);
+                }
+ 
+                // Если все ок возвращаем 1
+                return 1;
+ 
+            } else {
+                return null;
+            }
+ 
         } else {
+            // Если не удалось перезаписать в базе
             return null;
         }
     }
