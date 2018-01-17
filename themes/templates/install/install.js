ccfc66f141dc7d266cab960308664a80639a688f
avantis <uaruscom@gmail.com>
2018-01-18 00:37:38 +0200

    Add files via upload

--- themes/templates/install/install.js
+++ themes/templates/install/install.js
@@ -34,6 +34,8 @@ function registerInSeller() {
 }
  
 function startShop() {
+    //$("#loader-div").html("<div class=\'loader-fon\'><div class=\'loader-wrapper\'><div class=\'loader\'></div></div></div>");
+    $("#loader-link").html("<div class=\'load\'>Ожидайте !!! Добавляю товары и генерирую прайс-листы ...</div>");
     var csrf = $("#csrf").val()
     $.post("/start-shop", {csrf: csrf}, function (response) {
         var data = $.parseJSON(response)
