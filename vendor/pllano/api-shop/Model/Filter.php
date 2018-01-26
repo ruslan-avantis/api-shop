6d8ebd9cd18365facfaf46201c4ac637b00694a3
avantis <uaruscom@gmail.com>
2018-01-27 00:56:30 +0200

    Add files via upload

--- vendor/pllano/api-shop/Model/Filter.php
+++ vendor/pllano/api-shop/Model/Filter.php
@@ -113,7 +113,7 @@ class Filter {
     }
  
     // Пагинация
-    public function paginator($totalItems)
+    public function paginator_duo($totalItems)
     {
         $currentPage = $this->offset + 1; // Страница
         $itemsPerPage = $this->limit; // Товаров на странице
@@ -123,6 +123,8 @@ class Filter {
         $new_offset = $pagination->offset();
         $number_pages = intval($totalItems / $this->limit); // Вычисляем колличество страниц
         $paginator = '';
+        $previous = '';
+        $next = '';
         if ($number_pages >= 2) {
             foreach($pages as $key => $pag_item)
             {
@@ -140,30 +142,78 @@ class Filter {
                 if (isset($this->name)){$arr['search'] = $this->name;}
                 if (isset($this->alias)){$arr['alias'] = $this->alias;}
                 $pagination_url = $this->url_path.'?'.http_build_query($arr);
+                
                 $previous = '';
                 $next = '';
                 if ($pag_item == 'first') {
                     $previous = '<li class="page-item"><a class="page-link" href="'.$pagination_url.'">&larr; '.$real_key.'</a></li>';
-                }
-                if ($pag_item == 'less') {
+                } elseif ($pag_item == 'less') {
                     $previous = '<li class="page-item"><a class="page-link" href="'.$pagination_url.'">'.$real_key.'</a></li>';
-                }
-                if ($pag_item == 'previous') {
+                } elseif ($pag_item == 'previous') {
                     $previous = '<li class="page-item"><a class="page-link" href="'.$pagination_url.'">'.$real_key.'</a></li>';
-                }
-                if ($pag_item == 'current') {
+                } elseif ($pag_item == 'current') {
                     $next = '<li class="page-item active"><a class="page-link" href="#">'.$real_key.'</a></li>';
-                }
-                if ($pag_item == 'next') {
+                } elseif ($pag_item == 'next') {
                     $next = '<li class="page-item"><a class="page-link" href="'.$pagination_url.'">'.$real_key.'</li>';
-                }
-                if ($pag_item == 'more') {
+                } elseif ($pag_item == 'more') {
                     $next = '<li class="page-item"><a class="page-link" href="'.$pagination_url.'">'.$real_key.'</a></li>';
-                }
-                if ($pag_item == 'last') {
+                } elseif ($pag_item == 'last') {
                     $previous = '<li class="page-item"><a class="page-link" href="'.$pagination_url.'">'.$real_key.' &rarr;</a></li>';
                 }
+                
                 $paginator .= $previous.''.$next;
+            }
+        }
+        return $paginator;
+    }
+ 
+    // Пагинация
+    public function paginator($totalItems)
+    {
+        $currentPage = $this->offset + 1; // Страница
+        $itemsPerPage = $this->limit; // Товаров на странице
+        $neighbours = 4; // Колличество линков по обе стороны от активного
+        $pagination = new Pagination($totalItems, $currentPage, $itemsPerPage, $neighbours);
+        $pages = $pagination->build(); // Contains associative array with a numbers of a pages
+        $new_offset = $pagination->offset();
+        $number_pages = intval($totalItems / $this->limit); // Вычисляем колличество страниц
+        $paginator = array();
+       
+        if ($number_pages >= 2) {
+            foreach($pages as $key => $pag_item)
+            {
+                $real_key = $key - 1;
+                $arr = array();
+                if ($real_key >= 1) {$arr['offset'] = $real_key;}
+                if (isset($this->order)){$arr['order'] = $this->order;}
+                if (isset($this->sort)){$arr['sort'] = $this->sort;}
+                if (isset($this->limit)){$arr['limit'] = $this->limit;}
+                if (isset($this->brand_id)){$arr['brand_id'] = $this->brand_id;}
+                if (isset($this->type)){$arr['type'] = $this->type;}
+                if (isset($this->brand)){$arr['brand'] = $this->brand;}
+                if (isset($this->serie)){$arr['serie'] = $this->serie;}
+                if (isset($this->articul)){$arr['articul'] = $this->articul;}
+                if (isset($this->name)){$arr['search'] = $this->name;}
+                if (isset($this->alias)){$arr['alias'] = $this->alias;}
+
+                $arrPag['key'] = $real_key;
+                $arrPag['url'] = $this->url_path.'?'.http_build_query($arr);    
+                if ($pag_item == 'first') {
+                    $arrPag['type'] = 'first';
+                } elseif ($pag_item == 'less') {
+                    $arrPag['type'] = 'less';
+                } elseif ($pag_item == 'previous') {
+                    $arrPag['type'] = 'previous';
+                } elseif ($pag_item == 'current') {
+                    $arrPag['type'] = 'current';
+                } elseif ($pag_item == 'next') {
+                    $arrPag['type'] = 'next';
+                } elseif ($pag_item == 'more') {
+                    $arrPag['type'] = 'more';
+                } elseif ($pag_item == 'last') {
+                    $arrPag['type'] = 'last';
+                }
+                $paginator[] = $arrPag;
             }
         }
         return $paginator;
--- vendor/pllano/api-shop/Resources/Menu.php
+++ vendor/pllano/api-shop/Resources/Menu.php
@@ -45,27 +45,76 @@ class Menu {
         $response = $db->get($this->resource, ["menu" => 1, "state" => 1, "sort" => "sort", "order" => "ASC", "offset" => 0, "limit" => 5]);
  
         if (isset($response['headers']['code'])) {
-			if ($response['headers']['code'] == 200 || $response['headers']['code'] == '200') {
-				
-				$resp = array();
-				foreach($response['body']['items'] as $value)
+            if ($response['headers']['code'] == 200 || $response['headers']['code'] == '200') {
+                
+                $resp = array();
+                foreach($response['body']['items'] as $value)
                 {
-					$val = $value['item'];
-					$item['alias'] = $val->alias;
-					$item['title'] = $val->title;
-					$item['title_ru'] = $val->title_ru;
-					$item['title_ua'] = $val->title_ua;
-					$item['title_en'] = $val->title_en;
-					$item['title_de'] = $val->title_de;
-					$item['url'] = $val->url;
-					$resp[] = $item;
-				}
+                    $val = $value['item'];
+                    if ($val->parent_id == 0) {
+                        $item['alias'] = $val->alias;
+                        $item['title'] = $val->title;
+                        $item['title_ru'] = $val->title_ru;
+                        $item['title_ua'] = $val->title_ua;
+                        $item['title_en'] = $val->title_en;
+                        $item['title_de'] = $val->title_de;
+                        $item['url'] = $val->url;
+ 
+                        $parent = $db->get($this->resource, ["menu" => 1, "state" => 1, "parent_id" => $val->id]);
+                        if (isset($parent['headers']['code'])) {
+                            if ($parent['headers']['code'] == 200 || $parent['headers']['code'] == '200') {
+                                foreach($parent['body']['items'] as $subvalue)
+                                {
+                                    $sub = $subvalue['item'];
+                                    $submenu['alias'] = $sub->alias;
+                                    $submenu['title'] = $sub->title;
+                                    $submenu['title_ru'] = $sub->title_ru;
+                                    $submenu['title_ua'] = $sub->title_ua;
+                                    $submenu['title_en'] = $sub->title_en;
+                                    $submenu['title_de'] = $sub->title_de;
+                                    $submenu['url'] = $sub->url;
+ 
+                                    $subparent = $db->get($this->resource, ["menu" => 1, "state" => 1, "parent_id" => $sub->id]);
+                                    if (isset($subparent['headers']['code'])) {
+                                        if ($subparent['headers']['code'] == 200 || $subparent['headers']['code'] == '200') {
+                                            $submenu['subsubmenu'] = '';
+                                            foreach($subparent['body']['items'] as $subsubvalue)
+                                            {
+                                                $subsub = $subsubvalue['item'];
+                                                $subsubmenu['alias'] = $subsub->alias;
+                                                $subsubmenu['title'] = $subsub->title;
+                                                $subsubmenu['title_ru'] = $subsub->title_ru;
+                                                $subsubmenu['title_ua'] = $subsub->title_ua;
+                                                $subsubmenu['title_en'] = $subsub->title_en;
+                                                $subsubmenu['title_de'] = $subsub->title_de;
+                                                $subsubmenu['url'] = $subsub->url;
+                                                
+                                                $submenu['subsubmenu'][] = $subsubmenu;
+                                            }
+                                        } else {
+                                            $submenu['subsubmenu'] = null;
+                                        }
+                                    } else {
+                                        $submenu['subsubmenu'] = null;
+                                    }
 
+                                    $item['submenu'][] = $submenu;
+                                }
+                            } else {
+                            $item['submenu'] = null;
+                        }
+                        } else {
+                            $item['submenu'] = null;
+                        }
+                        
+                        $resp[] = $item;
+                    }
+                }
                 return $resp;
             } else {
                 return null;
             }
-		} else {
+        } else {
             return null;
         }
     }
