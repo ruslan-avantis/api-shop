c08afea2d452daaaf04f97b63564cf9348da2a02
avantis <uaruscom@gmail.com>
2018-01-26 04:13:04 +0200

    Add files via upload

--- vendor/pllano/api-shop/Resources/Menu.php
+++ vendor/pllano/api-shop/Resources/Menu.php
@@ -10,18 +10,65 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
+ 
 namespace ApiShop\Resources;
-
+ 
+use ApiShop\Config\Settings;
+use RouterDb\Db;
+use RouterDb\Router;
+ 
 class Menu {
     
+    private $db_name;
+    private $resource = "category";
+    private $config;
+ 
     function __construct()
     {
+    // Подключаем конфиг Settings\Config
+    $config = (new Settings())->get();
+    $this->config = $config;
 
+    // Отдаем роутеру RouterDb конфигурацию.
+    $router = new Router($this->config);
+    // Получаем название базы для указанного ресурса
+    $this->db_name = $router->ping($this->resource);
+ 
     }
+ 
+    // Ресурс category доступен только на чтение
+    public function get()
+    {
+        // Подключаемся к базе
+        $db = new Db($this->db_name, $this->config);
+        // Отправляем запрос и получаем данные
+        $response = $db->get($this->resource, ["menu" => 1, "state" => 1, "sort" => "sort", "order" => "ASC", "offset" => 0, "limit" => 5]);
+ 
+        if (isset($response['headers']['code'])) {
+			if ($response['headers']['code'] == 200 || $response['headers']['code'] == '200') {
+				
+				$resp = array();
+				foreach($response['body']['items'] as $value)
+                {
+					$val = $value['item'];
+					$item['alias'] = $val->alias;
+					$item['title'] = $val->title;
+					$item['title_ru'] = $val->title_ru;
+					$item['title_ua'] = $val->title_ua;
+					$item['title_en'] = $val->title_en;
+					$item['title_de'] = $val->title_de;
+					$item['url'] = $val->url;
+					$resp[] = $item;
+				}
 
-    public function get() {
-
+                return $resp;
+            } else {
+                return null;
+            }
+		} else {
+            return null;
+        }
     }
+ 
 }
  
